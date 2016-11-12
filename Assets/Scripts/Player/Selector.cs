07523d3385d0096d8e4da77437ff82efda2787c6
Andrew Colannino <akcolannino@email.neit.edu>
2016-11-12 18:56:19 -0500

    Object selection

---
+++ Assets/Scripts/Player/Selector.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selector : MonoBehaviour
+{
+	Camera cam;
+	Pointer pointer;
+	int pSize;
+
+	void Start()
+	{
+		cam = GetComponent<Camera>();
+		pointer = GameObject.FindWithTag("Pointer").GetComponent<Pointer>();
+		pSize = Pointer.size/2;
+	}
+
+	void Update()
+	{
+		RaycastHit hit;
+		Ray[] rays = new Ray[4]
+		{
+			cam.ScreenPointToRay(new Vector3(Screen.width/2 - pSize, Screen.height/2 - pSize)),
+			cam.ScreenPointToRay(new Vector3(Screen.width/2 + pSize, Screen.height/2 - pSize)),
+			cam.ScreenPointToRay(new Vector3(Screen.width/2 - pSize, Screen.height/2 + pSize)),
+			cam.ScreenPointToRay(new Vector3(Screen.width/2 + pSize, Screen.height/2 + pSize))
+		};
+
+		bool setHover = false;
+
+		for (int i = 0; i < 4; i++)
+		{
+			if (Physics.Raycast(rays[i], out hit))
+			{
+				Transform objectHit = hit.transform;
+
+				if (objectHit.tag == "Selectable")
+				{
+					setHover = true;
+					break;
+				}
+			}
+		}
+
+		if (setHover)
+		{
+			if (!pointer.IsHovering)
+				pointer.OnHover();
+		}
+		else if (pointer.IsHovering)
+		{
+			pointer.OffHover();
+		}
+	}
+}
---
+++ Assets/Scripts/UI/Pointer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Pointer : MonoBehaviour
+{
+	public static int size;
+
+	Image image;
+	bool isHovering = false;
+	public bool IsHovering { get { return isHovering; } }
+
+	void Start()
+	{
+		image = GetComponent<Image>();
+		size = (int)image.rectTransform.rect.width;
+	}
+
+	void Update()
+	{
+		
+	}
+
+	public void OnHover()
+	{
+		image.color = Color.yellow;
+		isHovering = true;
+	}
+
+	public void OffHover()
+	{
+		image.color = Color.white;
+		isHovering = false;
+	}
+}

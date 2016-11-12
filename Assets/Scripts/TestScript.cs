e4b379a58bab03bb427bf30d513ace61906358cf
Andrew Colannino <akcolannino@email.neit.edu>
2016-11-12 15:17:16 -0500

    Added the Unity project

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour
+{
+	
+	void Start()
+	{
+		print("This is a test");
+	}
+
+	void Update()
+	{
+		transform.position += Vector3.forward * Time.deltaTime;
+	}
+}

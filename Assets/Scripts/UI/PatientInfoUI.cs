cf84bf88260230d069d923d18362d84465fd84c1
Andrew Colannino <akcolannino@email.neit.edu>
2016-12-01 12:04:22 -0500

    canvas merge, read in any num of patients

--- Assets/Scripts/UI/PatientInfoUI.cs
+++ Assets/Scripts/UI/PatientInfoUI.cs
@@ -5,68 +5,42 @@ using UnityEngine.UI;
 public class PatientInfoUI : MonoBehaviour
 {
     bool isVisible = true;
+	const int patientNum = 3;
 
     // texties
-    public string[] pReason = new string[3];
-    public string[] pLikes = new string[3];
-    public string[] pBg = new string[3];
-    public string[] pOther = new string[3];
-    public string[] pName = new string[3];
-    public string[] pAge = new string[3];
-    public string[] pSex = new string[3];
+    public string[] pReason = new string[patientNum];
+    public string[] pLikes = new string[patientNum];
+    public string[] pBg = new string[patientNum];
+    public string[] pOther = new string[patientNum];
+    public string[] pName = new string[patientNum];
+    public string[] pAge = new string[patientNum];
+    public string[] pSex = new string[patientNum];
 
     //patient picture
-    public Sprite[] patient = new Sprite[3];
+    public Sprite[] patient = new Sprite[patientNum];
 
 
     void Start()
 	{
-        Image img = GameObject.Find("PImage").GetComponent<Image>();
-        Text rText = GameObject.Find("Reason").GetComponent<Text>();
-        Text lText = GameObject.Find("LD").GetComponent<Text>();
-        Text bText = GameObject.Find("Back").GetComponent<Text>();
-        Text oText = GameObject.Find("Other").GetComponent<Text>();
-        Text nText = GameObject.Find("Name").GetComponent<Text>();
-        Text aText = GameObject.Find("Age").GetComponent<Text>();
-        Text sText = GameObject.Find("Sex").GetComponent<Text>();
+        Image img = transform.Find("PImage").GetComponent<Image>();
+        Text rText = transform.Find("Reason").GetComponent<Text>();
+        Text lText = transform.Find("LD").GetComponent<Text>();
+        Text bText = transform.Find("Back").GetComponent<Text>();
+        Text oText = transform.Find("Other").GetComponent<Text>();
+        Text nText = transform.Find("Name").GetComponent<Text>();
+        Text aText = transform.Find("Age").GetComponent<Text>();
+        Text sText = transform.Find("Sex").GetComponent<Text>();
 
-        int rnd = Random.Range(0, 2);
+        int rnd = Random.Range(0, patientNum-1);
 
-        if (rnd == 0)
-        {   
-            img.sprite = patient[0];
-            rText.text = pReason[0];
-            lText.text = pLikes[0];
-            bText.text = pBg[0];
-            oText.text = pOther[0];
-            nText.text = pName[0];
-            aText.text = pAge[0];
-            sText.text = pSex[0];
-        }
-
-        if (rnd == 1)
-        {
-            img.sprite = patient[1];
-            rText.text = pReason[1];
-            lText.text = pLikes[1];
-            bText.text = pBg[1];
-            oText.text = pOther[1];
-            nText.text = pName[1];
-            aText.text = pAge[1];
-            sText.text = pSex[1];
-        }
-
-        if (rnd == 2)
-        {
-            img.sprite = patient[2];
-            rText.text = pReason[2];
-            lText.text = pLikes[2];
-            bText.text = pBg[2];
-            oText.text = pOther[2];
-            nText.text = pName[2];
-            aText.text = pAge[2];
-            sText.text = pSex[2];
-        }
+        img.sprite = patient[rnd];
+        rText.text = pReason[rnd];
+        lText.text = pLikes[rnd];
+        bText.text = pBg[rnd];
+        oText.text = pOther[rnd];
+        nText.text = pName[rnd];
+        aText.text = pAge[rnd];
+        sText.text = pSex[rnd];
     }
 
 	void Update()

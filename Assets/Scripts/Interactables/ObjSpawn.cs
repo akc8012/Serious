8822c1972a33c891e95d9b66c94d4e31b50e0622
RBrounell <rbrounell@gmail.com>
2016-12-06 08:50:05 -0500

    Obj Spawner

---
+++ Assets/Scripts/Interactables/ObjSpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjSpawnL : MonoBehaviour
+{
+    const int spawnNum = 2;
+    public GameObject[] spawnable = new GameObject[spawnNum];
+
+    void Start ()
+    {
+        int rnd = Random.Range(0, spawnNum);
+
+        if (rnd == 0)
+        {
+            Instantiate(spawnable[0], transform.position, transform.rotation);
+        }
+
+        if (rnd == 1)
+        {
+            Instantiate(spawnable[1], transform.position, transform.rotation);
+        }
+    }
+	
+	void Update ()
+    {
+	
+	}
+}
